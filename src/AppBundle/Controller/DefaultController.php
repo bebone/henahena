604cc90243c78d5480d3db44bf018fed23f092ec
astartsava <startseva_anna@yahoo.com>
2017-02-07 15:12:59 +0100

    page index

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,11 +8,33 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/index")
      */
     public function indexAction()
     {
 
-        return $this->render(':default:index.html.twig');
+        return $this->render('AppBundle::index.html.twig');
     }
+    /**
+     * @Route("/logement")
+     */
+     public function logementAction()
+    {
+        return $this->render('AppBundle::logement.html.twig');
+    }
+    
+     /**
+     * @Route("/bonsplans")
+     */
+     public function bonplansAction()
+    {
+        return $this->render('AppBundle::bonsplans.html.twig');
+    }
+
+    
+    
+    
 }
+
+
+   
