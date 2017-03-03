d1c4469743b14777a361a70fd2adc2b452326a22
Anaïs <anais.vidal@openmailbox.org>
2017-03-03 11:01:52 +0100

    Echappement html Format #30

--- src/AppBundle/Entity/Bonplan.php
+++ src/AppBundle/Entity/Bonplan.php
@@ -160,7 +160,8 @@ class Bonplan
      */
     public function getContenu()
     {
-        return $this->contenu;
+        $output = preg_replace_callback("/(&#[0-9]+;)/", function($m) { return mb_convert_encoding($m[1], "UTF-8", "HTML-ENTITIES"); }, ($this->contenu));
+        return $output;
     }
 
     /**
